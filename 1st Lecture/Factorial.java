db13775ef281f56bd936d24c5ceaa7a83274705a
Shivam <shivam89raj45@gmail.com>
2021-08-22 10:18:12 +0530

    Added the first programme

---
+++ 1st Lecture/Factorial.java
@@ -0,0 +1,25 @@
+package com.shivam;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    //Problem Statement- Write a program to print factorial of a number, also take input.
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Programme To Print Factorial Of A Number!");
+        System.out.print("Enter a number : ");
+        int num = input.nextInt();
+
+        //Calculating Factorial
+        int count = num;
+        int factorial = 1;
+        while (count > 1){
+            factorial *= count;
+            count--;
+        }
+        System.out.print("The factorial of " + num + " is : " + factorial);
+    }
+}
